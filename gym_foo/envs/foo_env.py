786a14398346d4e64e3889e2c47c1c6a3870ee87
Manavi Shukla <manavi139@gmail.com>
2019-05-14 14:17:29 +0530

    Update foo_env.py

--- gym_foo/envs/foo_env.py
+++ gym_foo/envs/foo_env.py
@@ -6,7 +6,15 @@ class FooEnv(gym.Env):
   metadata = {'render.modes': ['human']}
 
   def __init__(self):
-    ...
+    self.state = []
+		for i in range(3):
+		  self.state += [[]]
+			for j in range(3):
+				self.state[i] += ["-"]
+		self.counter = 0
+		self.done = 0
+		self.add = [0, 0]
+		self.reward = 0
   def step(self, action):
     ...
   def reset(self):
